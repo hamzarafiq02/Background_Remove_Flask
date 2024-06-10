dcb31abde5a971dfea073dfaa8c13a6b4a77c1b5
hamzarafiq02 <hamzarafiq9876@gmail.com>
2024-06-10 22:29:42 +0500

    add docker file

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
